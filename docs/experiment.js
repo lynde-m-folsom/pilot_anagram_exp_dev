f85bf2a2507f6670bda5888e91deb96012f4d62e
Lynde Folsom <80857202+lynde-m-folsom@users.noreply.github.com>
2025-02-28 15:21:23 -0800

    Modified the last page such that participants only need to enter the last five digits of their Prolific ID 	modified:   docs/experiment.js

--- docs/experiment.js
+++ docs/experiment.js
@@ -251,7 +251,7 @@ timeline.push(blockC);
 // End of the experiment pages ----------------
 const end_confirm_subjid = {
     type: jsPsychAnagrammer,
-    anagram: "Please confirm your Prolific ID",
+    anagram: "Please confirm your Prolific ID by entering the last five digit",
     //correct: subject_id,
     id: "end_confirm_subjid",  
     set: "end_confirm_subjid",
